38b0d178c94091cada1aaecdcd739784327da7f8
Deadalus3010 <deadalus3010@gmail.com>
2021-01-18 22:10:20 +0100

    1.3 Update
    - Refactoring
    - Small Bugg fixes

--- armamm.sh
+++ armamm.sh
@@ -14,11 +14,11 @@
 #
 
 #	ArmA 3 Workshop Content Directory
-workshoppath='/home/lgsm/.steam/SteamApps/workshop/content/107410/'
+workshoppath='/home/lgsm/.steam/steamapps/workshop/content/107410/'
 #	ArmA 3 Mods Directory
-modpath='/home/lgsm/serverfiles/mods'
+modpath='/home/lgsm/arma/serverfiles/mods'
 #	ArmA 3 Keys Directory
-keypath='/home/lgsm/serverfiles/keys'
+keypath='/home/lgsm/arma/serverfiles/keys'
 
 #	lgsm SteamCMD path & game gameid
 steamcmd='/home/lgsm/.steam/steamcmd/steamcmd.sh'
@@ -39,12 +39,30 @@ function lowercase() {
 	#creating empty strings for later use
 	local y="" workshop2=""
 
-	echo -e "\n   we lowercase everything inside your workshop path..."
+	echo -e "   we lowercase everything inside your workshop path..."
+
+	#################
+#	Experimental
+#	Finds every uppercase folder and file
+#	and writes them into an array
+#	In theory more efficient, because it only searches directly for uppercase things
+#	And doesnt checks every file
+
+#	readarray -d '' uppercase_array_list < <(find $workshoppath -type d,f -name "[[:upper:]]*" -print0)
+	
+#	It wasnt further developed, because it saved the full path into the array
+#	This means, when the folder was lowercased before the files / folders inside it
+#	The files / folders wouldnt be found, because the parent folder is lowercased and was saved uppercased in the array
+#	Which means, to use this method, I need to sort the array first
+#	I will currently use the method down below, even when its not the best way
+#	When this script fully works as intended, I will start optimizing it, so I dont waste CPU Cycles
+#	(Even when this is an small script)
+	#################
 
 	#Checks if it finds any uppercase directory or file
 	if  test -z "$(find $workshoppath -type d,f -name "[[:upper:]]*")"
 	then
-		echo "   ...already lowercase..."
+		echo -e "   ...already lowercase...\n"
 	else
 		echo "   ...working..."
 		#
@@ -61,35 +79,37 @@ function lowercase() {
 			do
 				#${stringZ:NUMBER} --> "Removes" the characters from the first index |  #${#string} --> gets the number of characters the string has
 				#Here we "substract" the path from the mod
-				#After this operation, y will contain only the mod informations and the uppercase found thing
-				#This way, we dont lowercase anything that is inside the workshop path
-				#We only lowercase the mod itself
+				#After this operation, y will contain only the mod folder structure and the capital letter found file / folder
+				#This way, we dont lowercase anything that is inside the workshop path 
+				# -> linux is case sensitive, which means, that the script cant find an folder which is e.g. normally uppercase
+				#We only lowercase the modpath itself
 				y="${x:${#workshoppath}}"
 			
 				#Here we lowercase the mod files / folders
 				declare -l y
 
-				#here we create the new path + lowercased mod
+				#Here we create the new path + lowercased mod
 				#Now we have solved one big problem:
 				#We did not lowercase the path to the mod itself
 				#We only lowercased the mod
 				workshop2=$workshoppath$y
 
 				#Now we lowercase the found mods without changing the modpath
-				mv -v $x $workshop2
+				mv $x $workshop2
 			done
 		done
 
 		echo -e "\n\n"
 
 		#
-		#	Here we gonna check if there are still are uppercase files / folders
+		#	Here we will check if there are still files/folders in capital letters
 		#	We also inform the user which files / folders are uppercase
 		#	So he can manually lowercase them
 		#
 		readarray -d '' stilllowercase_ar < <(find $workshoppath -name "[[:upper:]]*" -print0)
 
-		if [[ ${stilllowercase_ar[@]} ]]; then
+		if [[ ${stilllowercase_ar[@]} ]]
+		then
 
         	typeset -i i=0 max=${#stilllowercase_ar[*]}
 
@@ -100,6 +120,65 @@ function lowercase() {
 			done
 
 			echo -e "\n   $(tput setaf 1)ERROR:$(tput setaf 7) The script couldnt lowercase the files / folders above, please manually lowercase them\n"
+
+			typeset -i i=0 max=${#stilllowercase_ar[*]}
+
+				echo -e "   Do you want to remove them?\n   Answer with 1 (all) or 2 (select them by yourself) or 3 (no)\n"
+			unset yn
+			read -p "   ?:" yn
+
+			#Is the input either a 1 or 2?
+			if [[ ! $yn =~ ^[1-3]+$ ]]
+			then
+				#Warns the user that his input arent digits (only once)
+				echo -e "   $(tput setaf 1)ERROR:$(tput setaf 7) Please only use the digits 1 or 2!\n"
+				while [[ ! "$yn" =~ ^[1-3]+$ ]]
+				do
+					read -p "   ?:" yn
+				done
+
+			elif [[  "$yn" == "1" ]]
+			then
+
+				while (( i < max ))
+				do
+
+					echo -e "   $(tput setaf 2)File/Folder $i: ${stilllowercase_ar[$i]} $(tput setaf 3)was removed $(tput setaf 7)"
+					rm -r "${stilllowercase_ar[$i]}"
+
+					i=i+1
+				done
+
+			elif [[  "$yn" == "2" ]]
+			then
+
+			    while (( i < max ))
+				do
+					echo -e "   Number: $i | $(tput setaf 2)File/Folder: ${stilllowercase_ar[i]}$(tput setaf 7)"
+					i=i+1
+				done
+
+				echo -e "   What would you like to remove?\n   ?:"
+
+				#Here we can read multiple variables with spaces inbetween the inputs
+				#I cant output anything when using read in an "indexed mode", this is why there is an"   ?:" above
+				unset readinput_ar
+				read -ra readinput_ar
+
+				#Now we only remove the ones you mentioned above
+				for i in "${readinput_ar[@]}"
+				do
+					echo -e "\n   $(tput setaf 3)Index: $i | File/Folder: ${stilllowercase_ar[i]} | Was Removed $(tput setaf 7)"
+
+					rm -r "${stilllowercase_ar[$i]}"
+				done
+
+			elif [[ "$yn" == "3" ]]
+			then
+
+				echo -e "   skipped"
+
+			fi
 
     	fi
 		
@@ -112,7 +191,9 @@ function lowercase() {
 #	(Just to display stuff)
 #
 function countmods() {
-    echo -e "\n   found $(ls $workshoppath | wc -l) Mods and $(find $workshoppath -name '*.bikey' | wc -l) '.bikeys'"
+	#Counts all mods
+    echo -e "   Found $(ls $workshoppath | wc -l) Mods and $(find $workshoppath -name '*.bikey' | wc -l) '.bikeys' \n"
+
 }
 
 #
@@ -121,11 +202,13 @@ function countmods() {
 function readmods() {
 
 	local -n workshopid=$1
-   	typeset -i modid=0
+    typeset -i modid=0
 
 	echo -e "\n   Please enter the workshop content id..."
 	echo -e "   https://steamcommunity.com/sharedfiles/filedetails/?l=english&id="$(tput setaf 2)"450814997"$(tput setaf 7)"(cba3 example)"
 	echo -e "   when you are finished, type '1'\n"
+
+	#We fill the "workshop" array with mod IDs which are input by the user
 
 	while [ $modid != 1 ]
 	do
@@ -136,7 +219,7 @@ function readmods() {
 		if [[ ! $modid =~ ^[0-9]+$ ]]
 		then
 			#Warns the user that his input arent digits (only once)
-			echo -ne "   $(tput setaf 1)ERROR:$(tput setaf 7) Please only use digits\n" >&2
+			echo -ne "   $(tput setaf 1)ERROR:$(tput setaf 7) Please only use digits\n"
 			while [[ ! "$modid" =~ ^[0-9]+$ ]]
 			do
 				read -p "   id:" modid
@@ -146,9 +229,10 @@ function readmods() {
 		workshopid+=($modid)
 	done
 
-	#Deleting the exit call (last index) from the array
+    #Deleting the exit call (last index) from the array
 	#Best solution I had
-	unset workshopid[${#workshopid[*]}-1]
+    unset workshopid[${#workshopid[*]}-1]
+
 }
 
 #	
@@ -160,10 +244,10 @@ function dlworkshop() {
 
 	#Launches the readmods() method
 	#Grabs the workshopid array from readmods()
-	readmods workshop
+    readmods workshop
 
 	echo -e "\n   $(tput setaf 6)Now we are gonna download your mods..."
-	echo -e "   $(tput setaf 5)and afterwards we lowercase all files / folders...$(tput setaf 7)"
+	echo -e "   $(tput setaf 5)and afterwards we check if some files / folders are lowercase...$(tput setaf 7)"
 
 	typeset -i i=0 max=${#workshop[*]} j=${#workshop[*]}-1
 
@@ -175,11 +259,11 @@ function dlworkshop() {
 		#Here we download the workshop mods
 		"${steamcmd}" +login anonymous +workshop_download_item "${gameid}" "${workshop[$i]}" validate +quit
 
-		#Symlink mods into $modpath directory
-		#This saves huge amount of disk space
-		#Because, for each arma server you use, you only need to have a link to the modfiles inside the /mods directory
-		#And no mod files, e.g. 5 servers use the same mod folder, instead of having 5 different unique mod folders
-		ln -s "$workshoppath${workshop[$i]}" "$modpath"
+        #Symlink mods into $modpath directory
+        #This saves huge amount of disk space
+        #Because, for each arma server you use, you only need to have a link to the modfiles inside the /mods directory
+        #And no mod files, e.g. 5 servers use the same mod folder, instead of having 5 different unique mod folders
+        ln -s "$workshoppath${workshop[$i]}" "$modpath"
 
 		#Now we add one, so we download the next item
 		let i+=1
@@ -203,7 +287,7 @@ function addkeys() {
 
 	echo -e "\n   Which .bikeys do you want to add?\n   Only needed when you set \"verifySignatures = 2\" in \"arma3server.server.cfg\"\n   Answer with 1 (yes) / 2 (no)\n"
 
-	countmods
+    countmods
 
 	#We check the workshop path and only write the workshop mods inside this array
 	readarray -d '' wppdir_ar < <(find $workshoppath -mindepth 1 -maxdepth 1 -type d -print0)
@@ -228,8 +312,8 @@ function addkeys() {
 	typeset -i i=0 max=${#bikey_ar[*]}
 
 	while (( i < max ))
-	do															#To get the Modname, we grep the "name" string
-		echo -e "   $(tput setaf 2)Bikey $i: ${bikey_ar[$i]:${#workshoppath}}$(tput setaf 7) | $(tput setaf 3)$(grep -m 1 'name' ${modname_ar[$i]})$(tput setaf 7)" #We limit grep to "first found"
+	do															                               #To get the Modname, we grep the "name" string, we also limit grep to "first found"
+		echo -e "   $(tput setaf 2)Bikey $i: ${bikey_ar[$i]:${#workshoppath}}$(tput setaf 7) | $(tput setaf 3)$(grep -m 1 'name' ${modname_ar[$i]} | awk '{ print substr( $0, 9 )}' | awk '{ print substr( $0, 1, length($0)-3 )}')$(tput setaf 7)"
 
 		unset yn
 		read -p "   ?:" yn
@@ -238,7 +322,7 @@ function addkeys() {
 		if [[ ! $yn =~ ^[1-2]+$ ]]
 		then
 			#Warns the user that his input arent digits (only once)
-			echo -e "   $(tput setaf 1)ERROR:$(tput setaf 7) Please only use the digits 1 or 2!\n" >&2
+			echo -e "   $(tput setaf 1)ERROR:$(tput setaf 7) Please only use the digits 1 or 2!\n" 
 			while [[ ! "$yn" =~ ^[1-2]+$ ]]
 			do
 				read -p "   ?:" yn
@@ -246,14 +330,16 @@ function addkeys() {
 		elif [[  "$yn" == "1" ]]
 		then
 
-            		cp -u ${bikey_ar[$i]} $keypath #-u -> copy only when the SOURCE file is newer than the destination file or when the destination file is missing
+            cp -u ${bikey_ar[$i]} $keypath #-u -> copy only when the SOURCE file is newer than the destination file or when the destination file is missing
 
 			#Here we catch the cp response, to check if errors occured
 			if [ $? -ne 0 ]
 			then
 				echo -e "   $(tput setaf 1)ERROR:$(tput setaf 7) Couldnt copy the ${bikey_ar[$i]} file!"
 			fi
+
 		fi
+
 		i=i+1
 	done
 }
@@ -264,7 +350,7 @@ function addkeys() {
 #
 function removekeys() {
 
-	echo -e "\n   Which .bikeys do you want to remove?\n   Only needed when you set \"verifySignatures = 2\" in \"arma3server.server.cfg\"\n   $(tput setaf 1)DO NOT$(tput setaf 7) remove the a3.bikey (if there is any)   \n   Answer with 1 (yes = removes the bikey from the keys directory) / 2 (no)\n"
+    echo -e "\n   Which .bikeys do you want to remove?\n   Only needed when you set \"verifySignatures = 2\" in \"arma3server.server.cfg\"\n   $(tput setaf 1)DO NOT$(tput setaf 7) remove the a3.bikey (if there is any)   \n   Answer with 1 (yes = removes the bikey from the keys directory) / 2 (no)\n"
 
 	#We write all the .bikey paths in the kbikey_ar array | k = keys direcotory
 	#We also exclude the "a3.bikey", this is an important server file, without it, no one can join your server
@@ -275,18 +361,19 @@ function removekeys() {
 
 	while (( i < max ))
 	do
+
 		unset yn
 
 		#Here we try to find the .bikey in the workshoppath directory, so we can find the mod or meta.cpp to get the modname
-		mod_ar+=($(find $workshoppath -name ${kbikey_ar[i]:${#keypath}+1} | head -n 1))
+		mod_ar=($(find $workshoppath -name ${kbikey_ar[i]:${#keypath}+1} | head -n 1))
 
 		#This isnt the best solution to go back 2 directories, but I didnt found any better one
-		dirname=($(dirname "$(dirname "${mod_ar[i]}")"))
+		dirname=($(dirname "$(dirname "$mod_ar")"))
 
 		#Here we get the actual modname of the .bikey
-		modname_ar+=($(find $dirname -name "mod.cpp" -o -name "meta.cpp" | head -n 1))
+		modname_ar=($(find $dirname -name "mod.cpp" -o -name "meta.cpp" | head -n 1))
 
-		echo -e "   $(tput setaf 2)Bikey $i: ${kbikey_ar[$i]:${#keypath}}$(tput setaf 7) | $(tput setaf 3)$(grep -m 1 'name' ${modname_ar[$i]})$(tput setaf 7)" #We limit grep to "first found"
+		echo -e "   $(tput setaf 2)Bikey $i: ${kbikey_ar[$i]:${#keypath}}$(tput setaf 7) | $(tput setaf 3)$(grep -m 1 'name' $modname_ar | awk '{ print substr( $0, 9 )}' | awk '{ print substr( $0, 1, length($0)-3 )}')$(tput setaf 7)" #We limit grep to "first found"
 
 		read -p "   ?:" yn
 
@@ -294,16 +381,16 @@ function removekeys() {
 		if [[ ! $yn =~ ^[1-2]+$ ]]
 		then
 			#Warns the user that his input arent digits (only once)
-			echo -e "   $(tput setaf 1)ERROR:$(tput setaf 7) Please only use the digits 1 or 2!\n" >&2
+			echo -e "   $(tput setaf 1)ERROR:$(tput setaf 7) Please only use the digits 1 or 2!\n" 
 			while [[ ! "$yn" =~ ^[1-2]+$ ]]
 			do
 				read -p "   ?:" yn
 			done
 		elif [[  "$yn" == "1" ]]
 		then
-			rm ${kbikey_ar[$i]} #Here we delete the .bikey file, bye bye, you wont be missed :) (hopefully)
+            rm ${kbikey_ar[$i]} #Here we delete the .bikey file, bye bye, you wont be missed :) (hopefully)
 		fi
-		i=i+1
+		i+=1
 	done
 }
 
@@ -312,13 +399,12 @@ function removekeys() {
 #
 function updatemods() {
 
-	echo -e "\n   Which mods do you want to update?\n   Do it like so: 0 3 5 2 4 1 | When you want to update all, just press enter\n   The order is not important, but dont forget the spaces! Confirm with [enter]\n"
+	echo -e "\n   Which mods do you want to update?\n   Do it like so: 0 3 5 2 4 1\n   The order is not important, but dont forget the spaces! Confirm with [enter]\n"
 
 	#We check the workshop path and only write the workshop mods inside this array
 	readarray -d '' wppdir_ar < <(find $workshoppath -mindepth 1 -maxdepth 1 -type d -print0)
 	typeset -i i=0 max=${#wppdir_ar[*]}
 
-	#Here we can read multiple variables with spaces inbetween the inputs
 	declare -a readinput_ar
 
 	while (( i < max ))
@@ -330,13 +416,9 @@ function updatemods() {
 
 	echo -e "   What would you like to update?\n   ?:"
 
+	#Here we can read multiple variables with spaces inbetween the inputs
 	#I cant output anything when using read in an "indexed mode", this is why there is an"   ?:" above
 	read -ra readinput_ar
-
-	#if [[ "$readinput_ar" == "all" ]]
-	#then
-			
-	#fi
 
 	#Now we only update the ones you mentioned above
 	for i in "${readinput_ar[@]}"
@@ -347,7 +429,7 @@ function updatemods() {
 	done
 }
 
-#	
+#
 #	Cron Updates
 #	This function can get called for cron
 #	So the scripts checks all mods if they got an update
@@ -382,7 +464,7 @@ function cronupdatemods() {
 #	
 main() {
 
-	echo -e "\n ArmA 3 ez Mod Manager"
+	echo -e "\n ArmA 3 ez Mod Manager -- v1.3"
 
 	#
 	#	ArmA Mod Manager Main Menu
@@ -417,19 +499,20 @@ main() {
 						;;
 
 		h|help|*)    	echo -e "
-		h|help\t\t - Shows this help text, check the github page for more informations
-		dl|download\t - Download Mods
-		ak|addkeys\t - Prompts the user for each one .bikey file found inside the workshop directory, if he wants to add it to the server/keys directory
-		rk|removekeys\t - Prompts the user for each one .bikey file found inside the server directory, if he wants to remove it from the server/keys directory
-		co|count\t - Shows the amount of mods & bikey files
-		up|update\t - Prompts the user for each Mod it finds, if he wants to update it
-		cup|cronupdate\t - updates all Mods it finds, without any prompt, ideal for using with cron
-		lo|lowercase\t - Lowercases all mods\n" 
+   h|help\t  - Shows this help text, check the github page for more informations
+   dl|download\t  - Download Mods
+   ak|addkeys\t  - Prompts the user for each one .bikey file found inside the workshop directory, if he wants to add it to the server/keys directory
+   rk|removekeys  - Prompts the user for each one .bikey file found inside the server directory, if he wants to remove it from the server/keys directory
+   co|count\t  - Shows the amount of mods & bikey files
+   up|update\t  - Prompts the user for each Mod it finds, if he wants to update it
+   cup|cronupdate - updates all Mods it finds, without any prompt, ideal for using with cron
+   lo|lowercase\t  - Lowercases all mods\n" 
 						;;
 	esac
 
 
 	echo "Okay bye"
+
 }
 
 #	Launches the main method
